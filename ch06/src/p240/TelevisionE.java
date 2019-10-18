76f0cb7db428a77b155bea77376107274b1bebaf
java <java@DESKTOP-T5J9FJA>
2019-10-18 10:22:30 +0900

    2017.10.18

---
+++ ch06/src/p240/TelevisionE.java
@@ -0,0 +1,8 @@
+package p240;
+
+public class TelevisionE {
+	public static void main(String[] args) {
+		System.out.println(Television.info);
+	}
+
+}
---
+++ ch06/src/p247/EarthE.java
@@ -0,0 +1,9 @@
+package p247;
+
+public class EarthE {
+	public static void main(String[] args) {
+		System.out.println("지구의 반지름: "+Earth.EARTH_RADIUS+"km");
+		System.out.println("지구의 반지름: "+Earth.EARTH_SURFACE_AREA+"km^2");
+	}
+
+}
